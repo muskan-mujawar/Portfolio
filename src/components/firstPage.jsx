da4691bc564ef7476ad53a10e448a365f92bd2e5
Muskan Mujawar <muskanmujawar2503@gmail.com>
2024-08-25 13:39:18 +0530

    added icons and profile links

--- src/components/firstPage.jsx
+++ src/components/firstPage.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { IoLogoLinkedin } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
+import { FaFileAlt } from "react-icons/fa";
 
 export default function FirstPage() {
   return (
@@ -10,10 +13,36 @@ export default function FirstPage() {
         <h1 className="name">MUSKAN MUJAWAR</h1>
         <p>
           {" "}
-          a Undergrad student pursuing a degree in Electronics and
-          Telecommunication.I am passionate about web development.Apart from
-          technical stuff i am also interested in music and Photography.
+          An undergrad student pursuing a degree in Electronics and
+          Telecommunication. I'm passionate about web development. Apart from
+          technical stuff, I am also interested in music and photography.
         </p>
+        <div className="links">
+          <a
+            className="btn btn-dark me-2"
+            target="_blank"
+            href="https://www.linkedin.com/in/muskan-mujawar-231a04323/"
+          >
+            <IoLogoLinkedin size={20} className="me-2" />
+            LinkedIn
+          </a>
+          <a
+            className="btn btn-dark me-2"
+            target="_blank"
+            href="https://github.com/muskan-mujawar"
+          >
+            <FaGithub className="me-2" />
+            GitHub
+          </a>
+          <a
+            className="btn btn-dark"
+            target="_blank"
+            href="https://rxresu.me/muskanmujawar2021.etc/resume"
+          >
+            <FaFileAlt className="me-2" />
+            Resume
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="py-3 my-4">
       <p className="text-center padding">
-        Desinged & Developed by &nbsp;
+        Designed & Developed by &nbsp;
         <span className="text-color">Muskan Mujawar</span>
       </p>
     </footer>
